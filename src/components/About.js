dbd68c4e003e20c82ef1d4eee333fd5c9da891fd
Aayush <aayush.khunger217@gmail.com>
2023-09-11 02:58:04 +0530

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,50 +1,28 @@
 import React, { Fragment } from 'react'
 
 function About() {
-  return (
-    <Fragment>
-        <div className="col py-3 d-flex align-items-center about-image"> 
-
-            <div className="container">
-                <div className="row">
-
-
-
-
-                    <div className="col-lg-6 col-md-12 col-sm-12 d-flex align-items-center">
-                        <div>
-                            <h1 className="font-big text-red slide-in-top">About me</h1>
-                            <hr></hr>
-                            <p className='slide-in-bottom'>Ambitious full-stack developer, a self-led learner and team player  <br></br>
-                                with one year of experience in web developement.
-                            </p>
-                            <p className='slide-in-bottom'>
-                                Currently looking for new opportunities in industry and willing<br></br>
-                                to work on challenging and diverse projects with the latest technologies.
-                            </p>
-                            <p className='slide-in-bottom'>
-                                Previously worked as Full stack web developer.<br></br>
-                                The technology I've worked on : <span className="text-red ls-2">Asp.Net Core, Asp.Net Mvc, <br></br>
-                                C#, Sql, Ssms, Telerik-UI, Html, Css, Js, Bootstrap </span>
-                            </p>
-                            <p className='slide-in-bottom'>
-                                Currently Learning React Js Library
-                            </p>
+    return (
+        <Fragment>
+            <div className="col py-3 d-flex align-items-center about-image">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-12 col-sm-12 d-flex align-items-center">
+                            <div>
+                                <h1 className="font-big text-red slide-in-top">About me</h1>
+                                <hr></hr>
+                                <p className='slide-in-bottom'>
+                                    Energetic and goal-oriented, I'm Aayush Khunger, a Computer Science graduate with a CGPA of 8.12 from SJBIT.<br></br> Currently a Software Engineer at ThoughtClan Technologies, I've swiftly transitioned from intern to a key contributor, specializing in React.js and Node.js. Prior roles at MyFinfi and ThoughtClan Technolgies as an intern have fortified my expertise in API development and UI design. <br></br>Proficient in MERN stack, Python, Java, and C++, I'm eager to drive impactful solutions and contribute effectively to innovative tech teams.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-12 col-sm-12">
+                            <img className="img-fluid" src="../images/nomad-amico.png" alt="..."></img>
                         </div>
                     </div>
-                    <div className="col-lg-6 col-md-12 col-sm-12">
-                        <img className="img-fluid" src="../images/nomad-amico.png" alt="..."></img>
-                    </div>
-
-
-
                 </div>
             </div>
-
-            
-        </div>
-    </Fragment>
-  )
+        </Fragment>
+    )
 }
 
 export default About
